300b1bb59a3c2ce37b37d226d8945baa547bb237
Rafael Menezes Barboza <ra29fa@gmail.com>
2021-06-16 13:39:46 -0300

    Update keystone.sh

--- keystone.sh
+++ keystone.sh
@@ -1,35 +1,15 @@
 #!/bin/bash
 set -x #echo on
 
-################################################################
-#sudo mysql -u root -p
-#	CREATE DATABASE keystone;
-#	GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'localhost' IDENTIFIED BY 'KEYSTONE_DBPASS';
-#	GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'%' IDENTIFIED BY 'KEYSTONE_DBPASS';
-#	exit
-
 mysql --user="root" --password="password" --execute="CREATE DATABASE IF NOT EXISTS keystone;"
 mysql --user="root" --password="password" --execute="GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'localhost' IDENTIFIED BY 'KEYSTONE_DBPASS';"
 mysql --user="root" --password="password" --execute="GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'%' IDENTIFIED BY 'KEYSTONE_DBPASS';"
-#################################################################
-	
-apt install keystone -y
 
-################################################################
-#sudo nano /etc/keystone/keystone.conf 
-#
-#	[database]
-#	# ...
-#	connection = mysql+pymysql://keystone:KEYSTONE_DBPASS@127.0.0.1/keystone 
-#	
-#	
-#	[token]
-#	# ...
-#	provider = fernet
+apt install keystone -y
 
 sed -i '/\[database\]$/a connection = mysql+pymysql://keystone:KEYSTONE_DBPASS@127.0.0.1/keystone' /etc/keystone/keystone.conf 
 sed -i '/\[token\]$/a provider = fernet' /etc/keystone/keystone.conf 
-############################################################	
+
 sudo su -s /bin/sh -c "keystone-manage db_sync" keystone
 keystone-manage fernet_setup --keystone-user keystone --keystone-group keystone
 keystone-manage credential_setup --keystone-user keystone --keystone-group keystone
