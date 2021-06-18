9f9367064f433a70d896f94fbe0eee74a15546eb
rmmenezes <ra29fa@gmail.com>
2021-06-18 10:29:12 -0300

    .

--- run-on-debian.sh
+++ run-on-debian.sh
@@ -3,11 +3,13 @@ set -x #echo on
 
 mv /etc/apt/sources.list /etc/apt/sources.list.original
 
-mv /files/debian/sources.list /etc/apt/sources.list 
+mv ./files/debian/sources.list /etc/apt/sources.list 
 
-apt-get install gnupg
+sudo DEBIAN_FRONTEND=noninteractive apt-get update -y
+sudo DEBIAN_FRONTEND=noninteractive apt-get install gnupg -y
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 0B91000371B127C2FF62A62781DCD8423B6F61A6
 
-apt-get update -y
-apt-get upgrade -y
+sudo DEBIAN_FRONTEND=noninteractive apt-get update -y
+sudo DEBIAN_FRONTEND=noninteractive 
+sudo DEBIAN_FRONTEND=noninteractive apt-get upgrade -y
 sudo DEBIAN_FRONTEND=noninteractive apt dist-upgrade -y
