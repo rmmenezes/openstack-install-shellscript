35595fa4812bfe0619bd03cfa0348d831db5b0e8
rmba <rmba@rafael.barboza>
2021-07-12 19:55:08 -0300

    .

--- default.sh
+++ default.sh
@@ -38,3 +38,20 @@ EOF
 systemctl enable etcd
 systemctl restart etcd
 
+apt install mariadb-server python3-pymysql -y
+touch /etc/mysql/mariadb.conf.d/99-openstack.cnf
+cat > /etc/mysql/mariadb.conf.d/99-openstack.cnf << EOF
+[mysqld]
+bind-address = 0.0.0.0 
+
+default-storage-engine = innodb
+innodb_file_per_table = on
+max_connections = 4096
+collation-server = utf8_general_ci
+character-set-server = utf8
+EOF
+
+service mysql restart
+sudo systemctl restart mysql
+sudo systemctl restart mariadb
+
