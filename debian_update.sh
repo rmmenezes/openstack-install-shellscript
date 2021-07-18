7aba44738fa2e4381b053b43a175ee92077034ec
Rafael Menezes Barboza <ra29fa@gmail.com>
2021-07-18 16:36:02 -0300

    Revert "."
    This reverts commit 85eb26745e630bf829309d83a39343a53ea7fb6c.

--- debian_update.sh
+++ debian_update.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 set -x #echo on
 
-sudo DEBIAN_FRONTEND=noninteractive apt-get install gnupg -yq
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 0B91000371B127C2FF62A62781DCD8423B6F61A6
-
 mv /etc/apt/sources.list /etc/apt/sources.list.original
+
 cat ./files/debian/sources.list > /etc/apt/sources.list 
 
 sudo DEBIAN_FRONTEND=noninteractive apt-get install gnupg -yq
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 0B91000371B127C2FF62A62781DCD8423B6F61A6
-
 sudo DEBIAN_FRONTEND=noninteractive apt-get update -yq
 sudo DEBIAN_FRONTEND=noninteractive apt-get upgrade -yq
